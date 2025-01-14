2c83a2a37626fe7810b9596c1dd69fc0d07c81af
baskar <forpayment908@gmail.com>
2025-01-14 18:39:41 +0530

    frist project updated

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from './componets/Layout/layout'
+import Logo from './componets/Layout/logo/Logo'
+
+function App() {
+  return (
+    <div>
+      <Layout/>
+      App</div>
+  )
+}
+
+export default App
